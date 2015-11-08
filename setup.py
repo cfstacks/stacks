1e48303c9f78d95217fa41d937e68503954ffab3
Vaidas Jablonskis <jablonskis@gmail.com>
2015-11-08 21:56:44 +0000

    More tests

--- setup.py
+++ setup.py
@@ -7,6 +7,19 @@ about = {}
 with open(os.path.join(base_dir, 'stacks', '__about__.py')) as f:
     exec(f.read(), about)
 
+install_requires = [
+    'configargparse>=0.9.3',
+    'PyYAML>=3.11',
+    'Jinja2>=2.7.3',
+    'boto>=2.38.0',
+    'botocore>=1.1.1',
+    'tabulate>=0.7.5',
+    'setuptools',
+]
+
+tests_require = [
+    'moto',
+]
 
 config = {
     'name': 'stacks',
@@ -15,15 +28,8 @@ config = {
     'download_url': about['__url__'],
     'version': about['__version__'],
     'packages': find_packages(),
-    'install_requires': [
-        'configargparse>=0.9.3',
-        'PyYAML>=3.11',
-        'Jinja2>=2.7.3',
-        'boto>=2.38.0',
-        'botocore>=1.1.1',
-        'tabulate>=0.7.5',
-        'setuptools',
-    ],
+    'install_requires': install_requires,
+    'tests_require': tests_require,
     'entry_points': {
         'console_scripts': [
             'stacks = stacks.__main__:main',
