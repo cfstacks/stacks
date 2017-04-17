45c54de6ae6daea000457979c830d232abbdc685
Vaidas Jablonskis <jablonskis@gmail.com>
2017-04-17 18:56:48 +0100

    New release v0.4.0

--- stacks/__about__.py
+++ stacks/__about__.py
@@ -1,5 +1,5 @@
-__version__ = '0.3.3'
+__version__ = '0.4.0'
 __licence__ = 'MIT'
-__url__ = 'https://cfstacks.github.io'
+__url__ = 'https://stacks.tools'
 __maintainer__ = 'Vaidas Jablonskis'
 __maintainer_email__ = 'jablonskis@gmail.com'
